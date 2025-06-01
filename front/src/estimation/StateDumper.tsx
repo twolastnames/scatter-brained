38c080c88767cd9701b25732fc10847fbe2e7b06
Alan Glenn <alanglenn16@outlook.com>
2025-06-01 10:16:51 -0500

    do not have a useless log output

--- front/src/estimation/StateDumper.tsx
+++ front/src/estimation/StateDumper.tsx
@@ -4,6 +4,5 @@ import { useEstimationEvent } from "./Event";
 export function StateDumper(): ReactNode {
   const state = useEstimationEvent((current) => current || {}, {});
   const text = JSON.stringify(state, null, 2);
-  console.log("textttttt", { text });
   return <span>{text}</span>;
 }
