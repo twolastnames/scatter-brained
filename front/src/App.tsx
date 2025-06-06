e46f19d922674341348656ef0981c3d5d481bed4
Alan Glenn <alanglenn16@outlook.com>
2025-06-06 17:21:46 -0500

    forward layout styling

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { StateDumper } from "./components/StateDumper/StateDumper";
 import styles from "./App.module.scss";
 import { CardSet } from "./components/CardSet/CardSet";
 import { useEffect } from "react";
+import { PeerTileSet } from "./components/PeerTileSet/PeerTileSet";
 
 function App() {
   useEffect(() => {
@@ -10,20 +11,25 @@ function App() {
   return (
     <>
       <div className={styles.page} data-testid="ScatterBrained">
-        <CardSet />
+        <div className={styles.estimation}>
+          <CardSet />
+          <div>
+            <input
+              name="themeToggle"
+              onClick={(event) => {
+                document.documentElement.dataset["theme"] = event.currentTarget
+                  .checked
+                  ? "dark"
+                  : "light";
+              }}
+              type="checkbox"
+              id="themeSwitch"
+            />
+            <label for="themeToggle">Dark Mode</label>
+          </div>
+          <PeerTileSet />
+        </div>
         <StateDumper />
-        <input
-          name="themeToggle"
-          onClick={(event) => {
-            document.documentElement.dataset["theme"] = event.currentTarget
-              .checked
-              ? "dark"
-              : "light";
-          }}
-          type="checkbox"
-          id="themeSwitch"
-        />
-        <label for="themeToggle">Dark Mode</label>
       </div>
     </>
   );
