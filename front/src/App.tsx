7fef881e21e216438f8510df35337798a09ea5ff
Alan Glenn <alanglenn16@outlook.com>
2025-06-07 11:07:11 -0500

    have controls section

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./App.module.scss";
 import { CardSet } from "./components/CardSet/CardSet";
 import { useEffect } from "react";
 import { PeerTileSet } from "./components/PeerTileSet/PeerTileSet";
+import { Identity } from "./components/Identity/Identity";
 
 function App() {
   useEffect(() => {
@@ -13,19 +14,22 @@ function App() {
       <div className={styles.page} data-testid="ScatterBrained">
         <div className={styles.estimation}>
           <CardSet />
-          <div>
-            <input
-              name="themeToggle"
-              onClick={(event) => {
-                document.documentElement.dataset["theme"] = event.currentTarget
-                  .checked
-                  ? "dark"
-                  : "light";
-              }}
-              type="checkbox"
-              id="themeSwitch"
-            />
-            <label for="themeToggle">Dark Mode</label>
+          <div className={styles.controls}>
+            <Identity />
+            <div>
+              <input
+                name="themeToggle"
+                onClick={(event) => {
+                  document.documentElement.dataset["theme"] = event
+                    .currentTarget.checked
+                    ? "dark"
+                    : "light";
+                }}
+                type="checkbox"
+                id="themeSwitch"
+              />
+              <label for="themeToggle">Dark Mode</label>
+            </div>
           </div>
           <PeerTileSet />
         </div>
