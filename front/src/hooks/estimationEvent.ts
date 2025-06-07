5e01ee9c84f57a4e3ecd49c9e10486851aa3e46c
Alan Glenn <alanglenn16@outlook.com>
2025-06-07 10:56:50 -0500

    remove unused estimation state fields

--- front/src/hooks/estimationEvent.ts
+++ front/src/hooks/estimationEvent.ts
@@ -3,20 +3,17 @@ import { useEffect, useState } from "react";
 
 export type Participant = {
   selected?: number;
-  lastHeartbeat: number;
   lurker: boolean;
   name: string;
 };
 
 export type EstimationState = {
-  displayed?: boolean;
   participants?: {
     [id: string]: Participant;
   };
 };
 
 export const initialEstimationState: EstimationState = {
-  displayed: false,
   participants: {},
 };
 
