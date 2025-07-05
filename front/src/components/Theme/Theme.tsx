0e9f4f2a5b02c94d12b6bc25ac1dc73f0afe05bb
Alan Glenn <alanglenn16@outlook.com>
2025-07-04 22:23:47 -0500

    have variating theme text

--- front/src/components/Theme/Theme.tsx
+++ front/src/components/Theme/Theme.tsx
@@ -1,20 +1,50 @@
-import { type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { Checkbox } from "../Checkbox/Checkbox";
 
+enum Themes {
+  dark = "dark",
+  light = "light",
+}
+
+const localStorageKey = "theme";
+
+const getStored = () => localStorage.getItem(localStorageKey);
+
+const setStored = (value: string) =>
+  localStorage.setItem(localStorageKey, value);
+
+if (!getStored()) {
+  setStored(Themes.light);
+}
+
+const initial = getStored() || Themes.light;
+
+const getOpposite = (theme: string) =>
+  theme == Themes.dark ? Themes.light : Themes.dark;
+
 const useThemeSwitcher = () => {
-  const returnable: [undefined, (arg: boolean) => void] = [
-    undefined,
+  const [checked, setChecked] = useState<boolean>(false);
+  const returnable: [boolean, (arg: boolean) => void] = [
+    checked,
     (checked: boolean) => {
-      document.documentElement.dataset["theme"] = checked ? "dark" : "light";
+      setChecked(checked);
+      const theme = checked ? getOpposite(initial) : initial;
+      setStored(theme);
+      document.documentElement.dataset["theme"] = theme;
     },
   ];
   return returnable;
 };
 
 export function Theme(): ReactNode {
+  useEffect(() => {
+    setTimeout(() => {
+      document.documentElement.dataset["theme"] = initial || "light";
+    }, 0);
+  }, []);
   return (
     <Checkbox id="themeSwitcher" useStateTracker={useThemeSwitcher}>
-      Dark Mode
+      {initial === Themes.light ? "Dark Mode" : "Light Mode"}
     </Checkbox>
   );
 }
