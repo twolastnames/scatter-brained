0e0352a83b19d4340ff082d5fe7a52c685fca47c
Alan Glenn <alanglenn74@gmail.com>
2025-07-17 18:39:00 -0500

    have configurable cards

--- front/src/common/cardMap.ts
+++ front/src/common/cardMap.ts
@@ -1,1 +1,11 @@
-export const cardMap = ["1/2", "1", "2", "3", "5"];
+let cards = [1, 2, 3, 5];
+
+try {
+  cards = eval(import.meta.env.VITE_CARDS);
+} catch {
+  console.error(
+    `invalid VITE_CARDS value. good example: VITE_CARDS='["1/2",5,6]'`,
+  );
+}
+
+export const cardMap = cards;
